0100d0bb54adc260746ebdf1edc115a1fe5667fc
Gabriel Rodrigues <gr@gapp.im>
2018-05-16 18:15:38 -0300

    Checking if alternative is leaf

--- dam/src/main/java/goald/dam/model/Alternative.java
+++ dam/src/main/java/goald/dam/model/Alternative.java
@@ -12,5 +12,45 @@ public class Alternative {
 	private Boolean resolved;
 	
 	private List<Dame> listDepDame;
+
+	public Dame getParentDame() {
+		return parentDame;
+	}
+
+	public void setParentDame(Dame parentDame) {
+		this.parentDame = parentDame;
+	}
+
+	public List<String> getDependencyGoals() {
+		return dependencyGoals;
+	}
+
+	public void setDependencyGoals(List<String> dependencyGoals) {
+		this.dependencyGoals = dependencyGoals;
+	}
+
+	public Boolean getCtxSatisfied() {
+		return ctxSatisfied;
+	}
+
+	public void setCtxSatisfied(Boolean ctxSatisfied) {
+		this.ctxSatisfied = ctxSatisfied;
+	}
+
+	public Boolean getResolved() {
+		return resolved;
+	}
+
+	public void setResolved(Boolean resolved) {
+		this.resolved = resolved;
+	}
+
+	public List<Dame> getListDepDame() {
+		return listDepDame;
+	}
+
+	public void setListDepDame(List<Dame> listDepDame) {
+		this.listDepDame = listDepDame;
+	}
 	
 }
